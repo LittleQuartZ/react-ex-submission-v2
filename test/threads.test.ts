d4e3434716327392d86159a6b65ec2ad670041d0
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2022-11-24 08:51:17 +0700

    tests: add asyncGetAllThreads tests

--- test/threads.test.ts
+++ test/threads.test.ts
@@ -1,11 +1,49 @@
-import { describe, it, expect, beforeEach } from "vitest";
-import { type Thread } from "../src/utils/api";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import api, { type Thread } from "../src/utils/api";
 import store from "../src/states/index";
 import {
   clearThreads,
   setThreads,
   addThread,
 } from "../src/states/threads/actions";
+import { asyncGetAllThreads } from "../src/states/threads/thunks";
+
+const fakeThreads: Thread[] = [
+  {
+    id: "thread-1",
+    title: "Thread 1",
+    body: "Body of Thread 1",
+    createdAt: new Date().toISOString(),
+    ownerId: "user-1",
+    upVotesBy: [],
+    downVotesBy: [],
+    totalComments: 1,
+  },
+  {
+    id: "thread-2",
+    title: "Thread 2",
+    body: "Body of Thread 2",
+    createdAt: new Date().toISOString(),
+    ownerId: "user-2",
+    upVotesBy: [],
+    downVotesBy: [],
+    totalComments: 2,
+  },
+  {
+    id: "thread-3",
+    title: "Thread 3",
+    body: "Body of Thread 3",
+    createdAt: new Date().toISOString(),
+    ownerId: "user-3",
+    upVotesBy: [],
+    downVotesBy: [],
+    totalComments: 3,
+  },
+];
+
+const fakeError = {
+  error: "oops, an error happened",
+};
 
 describe("threadsSlice tests", () => {
   describe("actions", () => {
@@ -18,38 +56,7 @@ describe("threadsSlice tests", () => {
     it("should set threads", async () => {
       expect(getState().threads.list).toStrictEqual([]);
 
-      const threads: Thread[] = [
-        {
-          id: "thread-1",
-          title: "Thread 1",
-          body: "Body of Thread 1",
-          createdAt: new Date().toISOString(),
-          ownerId: "user-1",
-          upVotesBy: [],
-          downVotesBy: [],
-          totalComments: 1,
-        },
-        {
-          id: "thread-2",
-          title: "Thread 2",
-          body: "Body of Thread 2",
-          createdAt: new Date().toISOString(),
-          ownerId: "user-2",
-          upVotesBy: [],
-          downVotesBy: [],
-          totalComments: 2,
-        },
-        {
-          id: "thread-3",
-          title: "Thread 3",
-          body: "Body of Thread 3",
-          createdAt: new Date().toISOString(),
-          ownerId: "user-3",
-          upVotesBy: [],
-          downVotesBy: [],
-          totalComments: 3,
-        },
-      ];
+      const threads: Thread[] = fakeThreads;
       dispatch(setThreads(threads));
 
       expect(getState().threads.list).toStrictEqual(threads);
@@ -58,20 +65,61 @@ describe("threadsSlice tests", () => {
     it("should add one thread", () => {
       expect(getState().threads.list).toStrictEqual([]);
 
-      const thread: Thread = {
-        id: "1",
-        ownerId: "user-1",
-        title: "Thread 1",
-        body: "Body of Thread 1",
-        createdAt: new Date().toISOString(),
-        upVotesBy: [],
-        downVotesBy: [],
-        totalComments: 1,
-      };
+      const thread: Thread = fakeThreads[0];
 
       dispatch(addThread(thread));
 
       expect(getState().threads.list).toStrictEqual([thread]);
+    });
+  });
+
+  describe("thunks", () => {
+    const { dispatch, getState } = store;
+
+    vi.mock("../src/utils/api", () => ({
+      default: {
+        getAllThreads: vi.fn(),
+      },
+    }));
+    vi.stubGlobal("alert", vi.fn());
+
+    beforeEach(() => {
+      dispatch(clearThreads());
+    });
+
+    afterEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it("should fetch all threads and set threads", async () => {
+      expect(getState().threads.list).toStrictEqual([]);
+
+      vi.mocked(api.getAllThreads).mockImplementationOnce(() =>
+        Promise.resolve(fakeThreads)
+      );
+
+      await dispatch(asyncGetAllThreads());
+      expect(api.getAllThreads).toBeCalledTimes(1);
+
+      expect(getState().threads.list).toHaveLength(3);
+      expect(getState().threads.list).toStrictEqual(fakeThreads);
+    });
+
+    it("should fetch all threads and call alert", async () => {
+      expect(getState().threads.list).toStrictEqual([]);
+
+      vi.mocked(api.getAllThreads).mockImplementationOnce(() =>
+        Promise.resolve(fakeError)
+      );
+
+      await dispatch(asyncGetAllThreads());
+      expect(api.getAllThreads).toBeCalledTimes(1);
+
+      expect(alert).toBeCalledTimes(1);
+      expect(alert).toBeCalledWith(fakeError.error);
+
+      expect(getState().threads.list).toHaveLength(0);
+      expect(getState().threads.list).toStrictEqual([]);
     });
   });
 });
